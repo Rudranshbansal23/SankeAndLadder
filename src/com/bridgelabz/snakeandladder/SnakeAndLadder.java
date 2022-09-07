33c4c94ad2396312edd9e386698305f00f4d4866
Rudranshbansal23 <bansalrudransh2@gmail.com>
2022-09-07 16:52:46 +0530

    Added:start position zero

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		int position1 = 0;
+		System.out.println("Welcome to Snake and Ladder Game");
+		System.out.println("Position of Player1 is:" + position1);
+}
+}
